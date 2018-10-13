e4bfd1762599e8c4256b58dcc3e6a8500b2d6eb3
ansh <ansharora2299@gmail.com>
2018-10-14 03:58:36 +0530

    PATIENT LOGIN WITH DASHBOARD

---
+++ LOGIN WITHOUT UI, Patient Login Working/index1.php
@@ -0,0 +1,25 @@
+<?php
+$connection = mysqli_connect("localhost", "root", "");
+$db = mysqli_select_db($connection,"info");
+if (!$connection)
+{
+    die("not connected");
+}
+$customer_name=$_POST["customer_name"];
+$phone_number=$_POST["phone_number"];
+$email_address=$_POST["email_address"];
+$gender= $_POST["gender"];
+$dob=$_POST["dob"];
+$password=$_POST["password"];
+$ailment=implode(',',$_POST["ailment"]);
+$sql="insert into details (customer_name,phone_number,email_address,gender,dob,password,ailment) values('$customer_name',$phone_number,'$email_address','$gender',$dob,'$password','$ailment')";
+$i=mysqli_query($connection,$sql);
+if($i>0)
+{
+header("location:index.php");
+}
+   // check die part in this 
+   //$id= mysqli_insert_id($connection);
+   //echo $id;
+   //redirect request to another page
+?>
--- LOGIN WITHOUT UI/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$connection = mysqli_connect("localhost", "root", "");
-$db = mysqli_select_db($connection,"info");
-if (!$connection)
-{
-    die("not connected");
-}?>
