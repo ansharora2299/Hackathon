a4025011dd54b14cdf1c5589772acc2aeb4f18cf
Anay Khator <anaykhator@gmail.com>
2018-10-14 12:04:28 +0530

    Update Doctor login/assets/js/demo.js

--- Doctor login/assets/js/demo.js
+++ Doctor login/assets/js/demo.js
@@ -167,8 +167,20 @@ demo = {
 
         Chartist.Line('#chartHours', dataSales, optionsSales, responsiveSales);
 */
+
+
+var dbConnection = SQL.connect( { Driver: "MySQL",
+        Host: "localhost",
+        Database: "info",
+        UserName: "root",
+        Password: "" } );
+
+        var sql = "SELECT date FROM symptomes ";
+        var result = dbConnection.query(sql);
+        dbConnection.close();
+
 var chart = new Chartist.Line('.ct-chart', {
-  labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+  labels: result;
   series: [
     [12, 9, 7, 8, 5, 4, 6, 2, 3, 3, 4, 6],
     [4,  5, 3, 7, 3, 5, 5, 3, 4, 4, 5, 5],
